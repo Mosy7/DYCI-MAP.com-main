0e82eef916a9c18fbc1f2c49df722f39accc4162
SYMIE <74942457+Mosy7@users.noreply.github.com>
2024-05-04 06:08:31 +0800

    1

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCElh2CsFhEOJzWikw3hbHrh-08EXN1Jes",
   authDomain: "mapschedule-d22d5.firebaseapp.com",
@@ -16,7 +8,7 @@ const firebaseConfig = {
   appId: "1:271657628743:web:55ecd19dae3ee3f787ed62",
   measurementId: "G-Y5NXRCRDDF"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app=initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = firebase.auth();
+const database  = firebase.database();
--- mapscript.js
+++ mapscript.js
@@ -1,26 +1,41 @@
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCElh2CsFhEOJzWikw3hbHrh-08EXN1Jes",
-    authDomain: "mapschedule-d22d5.firebaseapp.com",
-    databaseURL: "https://mapschedule-d22d5-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "mapschedule-d22d5",
-    storageBucket: "mapschedule-d22d5.appspot.com",
-    messagingSenderId: "271657628743",
-    appId: "1:271657628743:web:55ecd19dae3ee3f787ed62",
-    measurementId: "G-Y5NXRCRDDF"
-  };
-  firebase.initializeApp(firebaseConfig);
+import firebaseConfig from './firebase-config.js';
+initializeApp(firebaseConfig);
 
-  //set data variable
-  var database = firebase.database
-
-
- function get(){
-    var building_ref = database.ref('Commerce Building'+'')
-    building_ref.on('value', function(snapshot){
-        var data = snapshot.val()
-
-        alert(data.rooms)
-    })
- }
+ 
+  function searchRoom() {
+    var searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    var searchResultsBody = document.getElementById('searchResultsBody');
+    var buildingAndRoom = document.getElementById('buildingAndRoom');
+  
+    // Clear previous search results only if there are existing ones
+    if (searchResultsBody.innerHTML.trim() !== '') {
+      searchResultsBody.innerHTML = '';
+    }
+    buildingAndRoom.innerHTML = '';
+  
+    database.ref('Buildings').orderByKey().on('value', function (snapshot) {
+      snapshot.forEach(function (building) {
+        building.child('rooms').forEach(function (room) {
+          var roomName = room.key.toLowerCase();
+          if (roomName.includes(searchTerm)) {
+            // Check if schedule exists before fetching
+            if (room.hasChild('schedule')) {
+              room.child('schedule').on('value', function (scheduleSnapshot) {
+                var scheduleHTML = '<tr><th>Schedule</th></tr>';
+                scheduleSnapshot.forEach(function (scheduleEntry) {
+                  scheduleHTML += '<tr><td>' + scheduleEntry.key + '</td><td>' + scheduleEntry.val() + '</td></tr>';
+                });
+                searchResultsBody.innerHTML += scheduleHTML;
+                buildingAndRoom.innerHTML = '<p>Building: ' + building.key + '</p><p>Room: ' + room.key + '</p>';
+              });
+            } else {
+              // Handle case where room has no schedule
+              console.log('Room ' + room.key + ' has no schedule');
+            }
+          }
+        });
+      });
+    });
+  }
